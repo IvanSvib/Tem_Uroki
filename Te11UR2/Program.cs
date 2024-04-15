47b2c1e429d3154f5e3ad98e2de28080a9e18480
IvanSvib <164069495+IvanSvib@users.noreply.github.com>
2024-04-15 23:44:30 +0300

    Добавьте файлы проекта.

---
+++ Te11UR2/Program.cs
@@ -0,0 +1,27 @@
+﻿
+//Домашнее задание. Тема 11. Урок 3. Условные операторы
+//и класс Random.
+
+
+Console.WriteLine("Введите число от 0 до 5. ");
+int x = int.Parse(Console.ReadLine());
+Random random = new Random();
+int y= random.Next(0,5);
+Console.WriteLine($"{y}");
+switch (x)
+{
+    case 1:
+        {
+            if (y == x )Console.WriteLine("victory");
+           else Console.WriteLine("defeat");
+
+        }
+        break;
+}
+
+
+
+
+
+
+
