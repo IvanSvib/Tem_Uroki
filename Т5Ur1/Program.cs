0b9a1300d28bcb6edb1d6b6bf26d9789d0d133b1
IvanSvib <164069495+IvanSvib@users.noreply.github.com>
2024-04-28 19:42:06 +0300

    Т5Ur1

---
+++ Т5Ur1/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Threading.Channels;
+
+Console.Write("Введите миллиметры для перевода:");
+double mm = double.Parse(Console.ReadLine());
+double san = mm * 0.1;
+double mil = mm * 1.35*10-7;
+double metr = mm * 0.001;
+double kilom = mm * 0.00001;
+double yard = mm * 0.001094;
+   Console.WriteLine($"{san:F2} см" +
+       $"\n{metr:F2}м." +
+       $"\n{kilom:F2}км." +
+       $"\n{mil:F2}миль." +
+       $"\n{yard:F2}ярдов.");
+
