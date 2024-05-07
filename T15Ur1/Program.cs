57fc79627236cea2b7643c53ebdb3ab605405ed1
IvanSvib <164069495+IvanSvib@users.noreply.github.com>
2024-05-07 21:28:55 +0300

    Домашнее задание. Тема 15. Урок 2. Одномерный массив.

--- T15Ur1/Program.cs
+++ T15Ur1/Program.cs
@@ -1,12 +1,27 @@
 ﻿
+//Домашнее задание. Тема 15. Урок 2. Одномерный
+//массив.
+int[] ints = new int[10];
+int rezalt = 0;
+for (int i = 0; i < 10; i++)
+{
+    Random random = new Random();
+    int n = random.Next(1,100);
+    ints[i] =  n ;
+    Console.WriteLine(ints[i]);
+    for (int j = 0; j < ints.Length; j++)
+    {
+        if (ints[i] % 2 == 0)
+        {
+            rezalt++;
+        }
+        break;
+    } 
+     
 
-st
 
-
-
-
-
-
+}
+Console.WriteLine($" Количество четных чисел = {rezalt}");
 
 
 
