960d770675b2ad4a2942abad65b05fb075dcf29a
IvanSvib <164069495+IvanSvib@users.noreply.github.com>
2024-05-09 23:51:38 +0300

    T15ur3

--- T15Ur1/Program.cs
+++ T15Ur1/Program.cs
@@ -1,27 +1,67 @@
 ﻿
 //Домашнее задание. Тема 15. Урок 2. Одномерный
-//массив.
-int[] ints = new int[10];
-int rezalt = 0;
+////массив.
+//int[] ints = new int[10];
+//int rezalt = 0;
+//for (int i = 0; i < 10; i++)
+//{
+//    Random random = new Random();
+//    int n = random.Next(1,100);
+//    ints[i] =  n ;
+//    Console.WriteLine(ints[i]);
+//    for (int j = 0; j < ints.Length; j++)
+//    {
+//        if (ints[i] % 2 == 0)
+//        {
+//            rezalt++;
+//        }
+//        break;
+//    } 
+
+
+
+//}
+//Console.WriteLine($" Количество четных чисел = {rezalt}");
+
+//Домашнее задание. Тема 15. Урок 3. Свойства и методы
+//одномерных массивов.
+
+//Напишите программу, которая запрашивает у пользователя 10 целых
+//чисел и сохраняет их в массиве. Затем программа сортирует массив по
+//возрастанию и выводит отсортированный массив на экран. Далее
+//программа запрашивает у пользователя число и находит его индекс в
+//массиве. Если число не найдено, программа выводит сообщение об
+//ошибке.
+
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+Console.WriteLine("Введите 10 чисел!");
+int[] mas = new int[10];
+int minNum = mas[0];
 for (int i = 0; i < 10; i++)
 {
     Random random = new Random();
-    int n = random.Next(1,100);
-    ints[i] =  n ;
-    Console.WriteLine(ints[i]);
-    for (int j = 0; j < ints.Length; j++)
+    int number = random.Next(1, 100);
+    mas[i] = number;
+    Console.WriteLine(mas[i]);
+}
+
+
+
+for (int i = 1; i < mas.Length; i++)
+{
+    if (mas[i] < minNum)
     {
-        if (ints[i] % 2 == 0)
-        {
-            rezalt++;
-        }
-        break;
-    } 
-     
+        minNum = mas[i];
+        Console.WriteLine(minNum);
+        
+    }
 
 
 }
-Console.WriteLine($" Количество четных чисел = {rezalt}");
+
+
+
 
 
 
---
+++ T15ur3/Program.cs
@@ -0,0 +1,42 @@
+﻿
+//Домашнее задание. Тема 15. Урок 3. Свойства и методы
+//одномерных массивов.
+//Напишите программу, которая запрашивает у пользователя 10 целых
+//чисел и сохраняет их в массиве. Затем программа сортирует массив по
+//возрастанию и выводит отсортированный массив на экран. Далее
+//программа запрашивает у пользователя число и находит его индекс в
+//массиве. Если число не найдено, программа выводит сообщение об
+////ошибке.
+Console.WriteLine("Введите 10 чисел!");
+int[] mas1 = new int[10];
+int[] mas2 = new int[10];
+
+for (int i = 0; i< 10; i++)
+{
+    Random random = new Random();
+    mas1[i] = random.Next(1,100);
+    Console.Write(mas1[i] + " ");
+
+    
+
+
+}
+Console.WriteLine();
+for (int i = 0; i < mas2.Length; i++)
+{
+    mas2 = mas1.OrderBy(i => i).ToArray();
+    Console.Write(mas2[i] + " ");
+}
+Console.WriteLine();
+
+for (int i = 0; i < mas2.Length; i++)
+{
+    Console.WriteLine(" Введите любое число из указанных выше!");
+    int number = int.Parse(Console.ReadLine());
+    int rezait = Array.IndexOf(mas2, number);
+    Console.WriteLine($"{rezait}"); 
+    if (rezait == -1)
+        Console.WriteLine("ОШИБКА!");
+}
+
+
